b90d9f284d0d1b0d5611320c075b1190caf10638
rafamar1 <rafaelmarquezgonzalez92@gmail.com>
2017-03-20 18:35:26 +0100

    -Creamos el proyecto HundirLaFlota -Añadimos Clases Main, Turno y Jugador, sin metodos

---
+++ HundirLaFlota/src/hundirlaflota/HundirLaFlota.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hundirlaflota;
+
+/**
+ *
+ * @author RafaMar
+ */
+public class HundirLaFlota {
+    
+    public static final int JUGADOR1 = 0;
+    
+    public static final int JUGADOR2 = 1;
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Jugador [] jugadores = new Jugador [2];
+        
+        Turno turno = new Turno(JUGADOR1);
+        
+        for (Jugador jugador : jugadores) {
+            
+        }
+        
+        
+    }
+    
+}
